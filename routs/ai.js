c7f838ef38fbcf8c68be5a9e8e0ac9eea5803c05
28Abhinav-Patidar <28abhipatidar@gmail.com>
2026-07-21 23:20:48 +0530

    fixing discription faliure

--- routs/ai.js
+++ routs/ai.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const controllerPath = require.resolve("../Controllers/ai");
 
-const aiController = require("../controllers/ai");
+const aiController = require("../Controllers/ai");
 
 const express = require("express");
 const router = express.Router();
